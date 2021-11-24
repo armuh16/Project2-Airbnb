1344bf64cac475c8990405176d0fe2e32bc8fe34
alfiancikoa <sikaraeng@gmail.com>
2021-11-24 22:53:43 +0800

    first_init

---
+++ routers/router.go
@@ -0,0 +1,12 @@
+package routers
+
+import "github.com/labstack/echo/v4"
+
+func New() *echo.Echo {
+	e := echo.New()
+	// ------------------------------------------------------------------
+	// LOGIN & REGISTER USER
+	// ------------------------------------------------------------------
+
+	return e
+}
