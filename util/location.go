bc22bf00fcc2d8ab64fee69786f5784a18dca57a
alfiancikoa <sikaraeng@gmail.com>
2021-11-27 20:29:10 +0800

    fix-location

--- util/location.go
+++ util/location.go
@@ -13,31 +13,19 @@ import (
 func GetGeocodeLocations(alamat string) (float64, float64, error) {
 	env := config.GetConfig()
 	apikey := env["APIKEYS"]
-	location := strings.ReplaceAll(alamat, " ", "+")
+	location := strings.ReplaceAll(alamat, " ", "%")
 	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/geocode/json?address=%v&key=%v", location, apikey)
 	response, err := http.Get(url)
 	if err != nil {
 		return 0, 0, err
 	}
-
 	body, err2 := ioutil.ReadAll(response.Body)
 	if err2 != nil {
 		return 0, 0, err
 	}
-
-	var longitude float64
-	var latitude float64
-	var values map[string]interface{}
-
-	json.Unmarshal(body, &values)
-	for _, value := range values["results"].([]interface{}) {
-		for str, arr := range value.(map[string]interface{}) {
-			if str == "geometry" {
-				latitude = arr.(map[string]interface{})["location"].(map[string]interface{})["lat"].(float64)
-				longitude = arr.(map[string]interface{})["location"].(map[string]interface{})["lng"].(float64)
-				break
-			}
-		}
-	}
-	return latitude, longitude, nil
+	var result Responses
+	json.Unmarshal([]byte(body), &result)
+	var lat float64 = result.Result[0].Geometry.Location.Lat
+	var lng float64 = result.Result[0].Geometry.Location.Lng
+	return lat, lng, nil
 }
