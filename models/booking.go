0041814a864849d6919d78b996283befd7ec35e8
Nathanael Novarian <nathanaelnovarian@gmail.com>
2021-12-01 15:32:26 +0700

    add status payment booking model

--- models/booking.go
+++ models/booking.go
@@ -7,16 +7,17 @@ import (
 )
 
 type Booking struct {
-	ID          int       `gorm:"primarykey;AUTO_INCREMENT"`
-	User_ID     int       `json:"userid" form:"userid"`
-	Homestay_ID int       `json:"homestayid" form:"homestayid"`
-	CheckIn     time.Time `gorm:"type:datetime;not null" json:"checkin" form:"checkin"`
-	CheckOut    time.Time `gorm:"type:datetime;not null" json:"checkout" form:"checkout"`
-	LongStay    int       `json:"longstay" form:"longstay"`
-	Total_Price int       `json:"totalprice" form:"totalprice"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	ID             int       `gorm:"primarykey;AUTO_INCREMENT"`
+	User_ID        int       `json:"userid" form:"userid"`
+	Homestay_ID    int       `json:"homestayid" form:"homestayid"`
+	CheckIn        time.Time `gorm:"type:datetime;not null" json:"checkin" form:"checkin"`
+	CheckOut       time.Time `gorm:"type:datetime;not null" json:"checkout" form:"checkout"`
+	LongStay       int       `json:"longstay" form:"longstay"`
+	Total_Price    int       `json:"totalprice" form:"totalprice"`
+	Status_Payment string    `gorm:"default:'not paided'" json:"statuspayment" form:"statuspayment"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      gorm.DeletedAt `gorm:"index"`
 }
 
 type BodyCheckIn struct {
